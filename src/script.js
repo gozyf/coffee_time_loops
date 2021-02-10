4da5a0ffbe3dcc4bc46893580d5a7a8b3412a0ac
Firuz Gozy <51522773+gozyf@users.noreply.github.com>
2021-02-10 10:59:05 +0800

    Add some new loops

--- src/script.js
+++ src/script.js
@@ -32,7 +32,7 @@ while (a <= 100) {
   a++;
 }
 
-5.	Write a  program to print all odd number between 1 to 100.
+//5.	Write a  program to print all odd number between 1 to 100.
 let a = 0;
 while (a <= 100) {
   if (a % 2 !== 0) 
@@ -40,7 +40,7 @@ while (a <= 100) {
   a++;
 }
 
-6.	Write a  program to find sum of all natural numbers between 1 to n.
+//6.	Write a  program to find sum of all natural numbers between 1 to n.
 
 let i = 1; 
 let n = 10 ; 
@@ -51,12 +51,12 @@ while (i<=n) {
 }
 console.log(sum);
 
-7.	Write a  program to find sum of all even numbers between 1 to n.
+//7.	Write a  program to find sum of all even numbers between 1 to n.
 
 
 
 
-8.	Write a  program to find sum of all odd numbers between 1 to n.
+//8.	Write a  program to find sum of all odd numbers between 1 to n.
 
 let i = 1; 
 let n = 5 ; 
@@ -68,7 +68,7 @@ while (i<=10) {
 
 
 
-9.	Write a  program to print multiplication table of any number.
+//9.	Write a  program to print multiplication table of any number.
 
 let table = 5;
 let count = 10;
@@ -78,7 +78,7 @@ while (i < count) {
     console.log(table, "X", i, "=", table * i);
 }
 
-10.	Write a  program to count number of digits in a number.
+//10.	Write a  program to count number of digits in a number.
 
 
 let count = 0;
@@ -88,6 +88,77 @@ while (n != 0)
     n = n / 10;
     count++;
 }
-
 console.log(count);
+
+
+
+//11.	Write a  program to find first and last digit of a number.
+
+let n = prompt('Enter the number:');
+let firstDigit = n;
+let lastDigit; 
+while(firstDigit >= 10 ){
+    firstDigit = firstDigit / 10; 
+    lastDigit = n % 10;     
+}
+console.log('This is the first digit:', firstDigit^0);
+console.log('This is the last digit:',lastDigit);
+
+//12.	Write a  program to find sum of first and last digit of a number.
+
+let n = prompt('Enter the number:');
+let firstDigit = n;
+let lastDigit; 
+while(firstDigit >= 10 ){
+    firstDigit = firstDigit / 10; 
+    lastDigit = n % 10;     
+}
+let sum = firstDigit + lastDigit;  
+console.log('This is the last digit:', sum^0);
+
+
+
+//13.	Write a  program to swap first and last digits of a number.
+
+let digit = prompt('Enter the digit');
+let firstDigit;
+let lastDigit; 
+let swappedNum;
+lastDigit = digit %  10;   //12334 -> 4 
+firstDigit = digit / 1000; //12334 -> 1 
+swappedNum = lastDigit * 10000;  //40000
+swappedNum = swappedNum + (digit % 10000); // 42334
+swappedNum = swappedNum - lastDigit; // 42330
+swappedNum = swappedNum + firstDigit; // 42331
+console.log(swappedNum^0);
+
+
+
+
+//14.	Write a  program to calculate sum of digits of a number.
+let num = prompt('Enter the number:');
+let sum = 0;
+while(num!=0)
+{
+    sum += num % 10;
+    num = num / 10;
+}
+console.log('The sum of digits:', sum^0);
+
+
+
+// 15.	Write a  program to calculate product of digits of a number.
+
+
+let num = prompt('Enter the number');
+let sum = 0;
+let product = 1; 
+while(num> 0){
+  const current = num % 10; 
+    product = product * current;
+    num = Math.floor(num / 10);
+}
+console.log(product - sum);
+
+
 */
